3b8f9bff336a50fbb1b1e9ad8601db421ae1599b
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-09-28 19:32:56 -0400

    use class-based components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -11,12 +12,23 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+   this.setState({
+    persons: [
+      {name: 'Maximilian', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Steph', age: 27}
+    ]
+   });
+
+  }
+
   render() {
     return (
       <div className="App">
        <h1>hi</h1>
        <p>this is really working</p>
-       <button>Switch Name</button>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing </Person>
        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
