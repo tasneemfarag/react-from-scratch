175152ec16fcb025dce96c463809540236a0d4cc
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-09-29 08:28:17 -0400

    using useState hook

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+const [personsState, setPersonsState] =  useState({
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Steph', age: 28}
     ]
+  });
+
+  const switchNameHandler = () => {
+    setPersonsState({
+     persons: [
+       {name: 'Maximilian', age: 28},
+       {name: 'Manu', age: 29},
+       {name: 'Steph', age: 27}
+     ]
+    });
   }
 
-  switchNameHandler = () => {
-   this.setState({
-    persons: [
-      {name: 'Maximilian', age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Steph', age: 27}
-    ]
-   });
-
-  }
-
-  render() {
     return (
       <div className="App">
        <h1>hi</h1>
        <p>this is really working</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing </Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+       <button onClick={switchNameHandler}>Switch Name</button>
+       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing </Person>
+       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
       </div>
     );
-  }
+  
 }
 
-export default App;
+export default app;
+
+
