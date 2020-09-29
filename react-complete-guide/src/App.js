c0981cc75934e3991ccc48cc3d06635b4e7006c0
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-09-29 15:12:49 -0400

    render content conditionally more dynamic

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -46,7 +46,26 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing </Person>
+          <Person
+            name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        </div>
+      );
     }
+
     return (
       <div className="App">
         <h1>hi</h1>
@@ -54,21 +73,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { 
-          this.state.showPersons ?
-          <div>
-            <Person
-              name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangedHandler}>My Hobbies: Racing </Person>
-            <Person
-              name={this.state.persons[2].name} age={this.state.persons[2].age} />
-          </div> : null
-        }
-
+        {persons}
       </div>
     );
   }
