feb8fc873d91b13f82eb4ff04f44789601d3f371
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-09-30 10:09:12 -0400

    deleting persons

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -13,15 +14,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Steph', age: 27 }
-      ]
-    });
-  }
   nameChangedHandler = (e) => {
     this.setState({
       persons: [
@@ -30,6 +22,13 @@ class App extends Component {
         { name: 'Steph', age: 26 }
       ]
     });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+   // persons.filter(person => person.index !== personIndex)
+   persons.splice(personIndex, 1);
+   this.setState({ persons: persons });
   }
 
   togglePersonsHandler = () => {
@@ -53,15 +52,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler}>My Hobbies: Racing </Person>
-          <Person
-            name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} />
+          })}
+
         </div>
       );
     }
