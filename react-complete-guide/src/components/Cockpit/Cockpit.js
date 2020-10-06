2d6718c668b6a6a30e2d8dee5ff58a9bcd5a8679
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-10-06 10:20:08 -0400

    cleanup Work with useEffect

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const Cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js]  useEffect');
+        // Http request....
+       setTimeout(()=> {
+            alert('Saved data to cloud!');
+        }, 1000);
+        return () => {
+            console.log('[Cockpit.js] clean up work in useEffect')
+        }
+
+    }, []);
+
+    useEffect(() => {
+        console.log('[Cockpit.js] 2nd useEffect');  
+        return () => {
+            console.log('[Cockpit.js] clean up work in 2nd useEffect');
+        } 
+    });
+
     const assignedClasses = [];
     let btnClass = '';
     if(props.showPersons) {
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -15,7 +15,8 @@ class App extends Component {
       { id: 'nmmk', name: 'Steph', age: 28 }
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -87,11 +88,12 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-       <Cockpit 
+       <button onClick={() => {this.setState({showCockpit: false})} }>Remove Cockpit</button> 
+       { this.state.showCockpit ? <Cockpit 
        title={this.props.appTitle}
        showPersons={this.state.showPersons} 
        persons={this.state.persons}
-       clicked={this.togglePersonsHandler} /> 
+       clicked={this.togglePersonsHandler} /> : null }
         {persons}
       </div>
     );
