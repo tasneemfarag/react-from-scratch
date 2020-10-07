d2790d3ed3f6b45c760783024b079af2d77892dc
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-10-07 15:46:06 -0400

    setting state correctly

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -56,9 +57,14 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
-  }
-
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
+  };
+  
   deletePersonHandler = (personIndex) => {
     //const persons = this.state.persons.slice(); this is right approch
     // persons.filter(person => person.index !== personIndex)
